b88b93650b9b1ba9539dbd5a6c50cc8e9d6e182e
Roshan Kumar <rsahu7989@gmail.com>
2025-06-10 00:19:31 +0530

    format eslint and tsconfig files for consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,12 @@ export default tseslint.config(
 	eslint.configs.recommended,
 	tseslint.configs.recommendedTypeChecked,
 	{
-		ignores: ['node_modules', 'dist', 'eslint.config.mjs', 'jest.config.js'],
+		ignores: [
+			'node_modules',
+			'dist',
+			'eslint.config.mjs',
+			'jest.config.js',
+		],
 	},
 	{
 		languageOptions: {
