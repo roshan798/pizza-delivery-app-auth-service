1c4cb27ea0d0b5254e37944e8ca415b29a1347a9
Roshan Kumar <rsahu7989@gmail.com>
2025-06-09 21:28:18 +0530

    setup logger

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
 import app from './app';
 import { Config } from './confiig';
+import logger from './confiig/logger';
 
 const startServer = () => {
 	const url = Config.URL;
 	const port = Config.PORT;
 	try {
 		app.listen(port, () => {
-			// eslint-disable-next-line no-console
-			console.log(`Server is running at ${url}`);
+			logger.info(`Server is running on ${url}`);
 		});
 	} catch (error) {
-		// eslint-disable-next-line no-console
-		console.error('Error starting the server:', error);
+		logger.error('Error starting the server:', error);
 		process.exit(1);
 	}
 };
