6b7c3bf19e8bf24b29f80d70810dbcaa54cb5cb3
Roshan Kumar <rsahu7989@gmail.com>
2025-06-10 21:32:47 +0530

    add supertest for API testing

--- app.spec.ts
+++ app.spec.ts
@@ -1,9 +1,15 @@
 import app from './src/app';
 import { getCurrentDateTime } from './src/utils';
+import request from 'supertest';
 
 describe('App', () => {
-	test('should be defined', () => {
+	it('should be defined', () => {
 		expect(app).toBeDefined();
+	});
+
+	it('should respond with 200 on GET /', async () => {
+		const response = await request(app).get('/');
+		expect(response.statusCode).toBe(200);
 	});
 });
 
