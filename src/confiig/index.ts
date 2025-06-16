13fff882ef1aa721da64d40a89f5a4bcd6f958cf
Roshan Kumar <rsahu7989@gmail.com>
2025-06-16 21:30:50 +0530

    add .dockerignore and Dockerfile for containerization

--- src/confiig/index.ts
+++ src/confiig/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080; // Default to 8080 if PORT is not set
 const HOST = process.env.HOST || 'localhost';
 
 export const Config = {
