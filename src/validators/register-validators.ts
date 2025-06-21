6ee24e183e7634e1b43213e90e73dabcee7d3b20
Roshan Kumar <rsahu7989@gmail.com>
2025-06-22 00:18:05 +0530

    converted to use schema based validation

--- src/validators/register-validators.ts
+++ src/validators/register-validators.ts
@@ -1,27 +1,30 @@
-import { body } from 'express-validator';
+import { checkSchema } from 'express-validator';
 import { AppDataSource } from '../confiig/data-source';
 import { User } from '../entity/User';
 import logger from '../confiig/logger';
 
-const registerValidator = [
-	body('email')
-		.notEmpty()
-		.withMessage('Email is Required!')
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		.custom(async (value: string, { req }) => {
-			const userRepository = AppDataSource.getRepository(User);
-			const existingUser = await userRepository.findOne({
-				where: { email: value },
-			});
-			if (existingUser) {
-				logger.warn(`User with email ${value} already exists`);
-				throw new Error('Email already exists');
-			}
-			logger.info(`No existing user found with email: ${value}`);
-			return true;
-		})
-		.trim()
-		.isEmail()
-		.withMessage('Invalid email format!'),
-];
-export default registerValidator;
+export default checkSchema({
+	email: {
+		isEmail: {
+			errorMessage: 'Invalid email format!',
+		},
+		notEmpty: {
+			errorMessage: 'Email is Required!',
+		},
+		custom: {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			options: async (email: string, { req }) => {
+				const userRepository = AppDataSource.getRepository(User);
+				const existingUser = await userRepository.findOne({
+					where: { email: email },
+				});
+				if (existingUser) {
+					logger.warn(`User with email ${email} already exists`);
+					throw new Error('Email already exists');
+				}
+				logger.info(`No existing user found with email: ${email}`);
+				return true;
+			},
+		},
+	},
+});
