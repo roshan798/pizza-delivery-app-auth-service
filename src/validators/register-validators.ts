fcb8b9b521e48278982a1c136fa56dbabca9608c
Roshan Kumar <rsahu7989@gmail.com>
2025-06-22 00:53:12 +0530

    add validation for first name, last name, and password in user registration

--- src/validators/register-validators.ts
+++ src/validators/register-validators.ts
@@ -8,7 +8,11 @@ export default checkSchema({
 		notEmpty: {
 			errorMessage: 'Email is Required!',
 		},
+
 		trim: true,
+		isEmail: {
+			errorMessage: 'Invalid email format!',
+		},
 		custom: {
 			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 			options: async (email: string, { req }) => {
@@ -25,4 +29,39 @@ export default checkSchema({
 			},
 		},
 	},
+	firstName: {
+		notEmpty: {
+			errorMessage: 'First Name is Required!',
+		},
+		trim: true,
+		isLength: {
+			options: { min: 2 },
+			errorMessage: 'First Name must be at least 2 characters long',
+		},
+	},
+	lastName: {
+		notEmpty: {
+			errorMessage: 'Last Name is Required!',
+		},
+		trim: true,
+		isLength: {
+			options: { min: 2 },
+			errorMessage: 'Last Name must be at least 2 characters long',
+		},
+	},
+	password: {
+		notEmpty: {
+			errorMessage: 'Password is Required!',
+		},
+		trim: true,
+		isLength: {
+			options: { min: 6 },
+			errorMessage: 'Password must be at least 6 characters long',
+		},
+		// isStrongPassword: {
+		// 	errorMessage:
+		// 		'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+		// },
+	},
+	// role validation will be added later
 });
