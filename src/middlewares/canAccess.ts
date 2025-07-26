260bdbdcbb67ea1df6061e23669f614bf5e98eb9
Roshan Kumar <rsahu7989@gmail.com>
2025-07-26 17:04:01 +0000

    add  logs

--- src/middlewares/canAccess.ts
+++ src/middlewares/canAccess.ts
@@ -1,20 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import { AuthRequest } from '../types';
+import logger from '../config/logger';
+
 export default function canAccess(roles: string[]) {
 	return (_req: Request, res: Response, next: NextFunction) => {
 		const req = _req as AuthRequest;
 		const roleFromRequest = req.auth?.role;
+
 		if (!roleFromRequest) {
+			logger.warn('Access denied: No role found in request');
 			res.status(403).json({ message: 'Access denied' });
 			return;
 		}
 
 		const hasAccess = roles.includes(roleFromRequest);
 		if (!hasAccess) {
+			logger.warn(
+				`Access denied: Role "${roleFromRequest}" is not authorized`
+			);
 			res.status(403).json({ message: 'Access denied' });
 			return;
 		}
 
+		logger.info(`Access granted: Role "${roleFromRequest}"`);
 		next();
 	};
 }
