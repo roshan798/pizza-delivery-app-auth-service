1dc711506634820f3d08f69a425e82bf71868551
Roshan Kumar <rsahu7989@gmail.com>
2025-08-16 16:11:43 +0530

    feat: implement createAdminUser function and corresponding tests

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,35 @@
-// examle method to get the current date and time
-export function getCurrentDateTime(): string {
-	const now = new Date();
-	return now.toISOString();
+import bcrypt from 'bcrypt';
+import { AppDataSource } from './config/data-source';
+import { User } from './entity/User';
+import logger from './config/logger';
+
+const HASH_SALT_ROUNDS = 10;
+export async function createAdminUser() {
+	const userRepo = AppDataSource.getRepository(User);
+
+	// check if admin already exists
+	const existingAdmin = await userRepo.findOne({
+		where: { role: 'admin' },
+	});
+
+	if (existingAdmin) {
+		logger.info('Admin user already exists.');
+		return;
+	}
+
+	const hashedPassword = await bcrypt.hash(
+		process.env.ADMIN_PASSWORD || 'admin123',
+		HASH_SALT_ROUNDS
+	);
+
+	const admin = userRepo.create({
+		firstName: 'Super',
+		lastName: 'Admin',
+		email: process.env.ADMIN_EMAIL || 'admin@system.local',
+		password: hashedPassword,
+		role: 'admin',
+	});
+
+	await userRepo.save(admin);
+	logger.info('Admin user created:', admin.email);
 }
