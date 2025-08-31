af40e12d0f7629d451dada50825b5e7f7e97ec7f
Roshan Kumar <rsahu7989@gmail.com>
2025-08-31 12:59:34 +0530

    remove console.log

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -35,7 +35,6 @@ export class UserController {
 			const tenant = await this.tenantService.getTenantById(
 				String(tenantId)
 			);
-			console.log(tenant);
 			if (!tenant) {
 				logger.warn(`[CREATE] Tenant with id ${tenantId} not found`);
 				return res.status(400).json({
