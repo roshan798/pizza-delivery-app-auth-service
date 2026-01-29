84cea9bd9ed32a7a9f2b3367235fd299ad0eb150
Roshan Kumar <rsahu7989@gmail.com>
2026-01-29 21:54:34 +0530

    feat: add user details to AuthRequest interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -28,6 +28,9 @@ export interface AuthRequest extends Request {
 		sub: string;
 		role: string;
 		tenantId?: string;
+		firstName: string;
+		lastName: string;
+		email: string;
 		iat?: number;
 		exp?: number;
 		iss?: string;
