a43fa6216f7793c9d05d72746d54552500a49192
Roshan Kumar <rsahu7989@gmail.com>
2026-03-01 20:29:53 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(
 app.get('/', (req, res) => {
 	res.json({
 		message: 'Welcome to Auth-Service From k8s 👋',
-		msg: 'This is the authentication service for the pizza delivery app. It handles user authentication, authorization, and tenant management.',
+		status: true,
 	});
 });
 app.use(express.json());
